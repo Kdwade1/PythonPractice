444a8fe7d4f16b101649addbe1fe78f8e778aeae
Kelvin <kelvin.daquon.wade@gmail.com>
2023-04-10 21:41:23 -0500

    python rock paper scissors

---
+++ RockPaperScissor.py
@@ -0,0 +1,31 @@
+import random
+
+playAgain = 'y'
+
+
+def play():
+    while playAgain == 'y':
+        user = input("Chose 'r' for rock,'p' for paper, 's' for scissor ")
+        computer = random.choice(['r', 'p', 's'])
+
+        if user == computer:
+            print(computer)
+            return "It's a tie"
+
+        if is_win(user, computer):
+            print(computer)
+            return "you win"
+        else:
+            print(computer)
+            return "you lost"
+
+
+# playAgain = input("would you like to play again? y/n ")
+
+
+def is_win(player, foe):
+    if (player == 's' and foe == 'p') or (player == 'p' and foe == 'r') or (player == 'r' and foe == 's'):
+        return True
+
+
+print(play())
